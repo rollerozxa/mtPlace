710aa02e30a681d9e1893a414dec6df5a0425ba9
ROllerozxa <rollerozxa@voxelmanip.se>
2023-02-23 22:36:48 +0100

    Improve mapgen, make size 256x256 and enclose it in invisible barries nodes

--- mods/place/mapgen.lua
+++ mods/place/mapgen.lua
@@ -1,8 +1,21 @@
+
+minetest.register_node("place:barrier", {
+	description = "Invisible Barrier",
+	drawtype = "airlike",
+	paramtype = "light",
+	pointable = false,
+	diggable = false,
+	buildable_to = false,
+	sunlight_propagates = true,
+	groups = { not_in_creative_inventory = 1 }
+})
+
 local node = minetest.get_content_id("place:colour_1")
+local barrier = minetest.get_content_id("place:barrier")
 
 local data = {}
 
-local size = 500
+local size = 128
 
 if minetest.get_mapgen_setting('mg_name') == "singlenode" then
 	minetest.register_on_generated(function(minp, maxp, blockseed)
@@ -12,11 +25,20 @@ if minetest.get_mapgen_setting('mg_name') == "singlenode" then
 
 		for z = minp.z, maxp.z do
 		for y = minp.y, maxp.y do
-		for x = minp.x, maxp.x do
-			if y == 0 and (x >= -size and x <= size) and (z >= -size and z <= size)  then
-				data[area:index(x, y, z)] = minetest.get_content_id("place:colour_"..math.random(23,23))
+			local posi = area:index(minp.x, y, z)
+			for x = minp.x, maxp.x do
+				if (x >= -size and x <= size) and (z >= -size and z <= size) then
+					if y == 0 then
+						data[posi] = minetest.get_content_id("place:colour_"..math.random(1,3))
+					elseif y < 0 then
+						data[posi] = barrier
+					end
+				else
+					data[posi] = barrier
+				end
+
+				posi = posi + 1
 			end
-		end
 		end
 		end
 
