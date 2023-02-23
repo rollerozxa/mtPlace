f36f1f3b843a0a2af9ad98d9c82a9aad7e1a9532
ROllerozxa <rollerozxa@voxelmanip.se>
2023-02-23 22:37:11 +0100

    Give players fly and fast on join, make sky less weird

--- mods/place/player.lua
+++ mods/place/player.lua
@@ -7,27 +7,35 @@ minetest.register_on_joinplayer(function(player)
 		player:get_inventory():set_stack("main", i, ItemStack("place:colour_"..i))
 	end
 
-	player:set_sky({
-		base_color = "#777788",
+	player:set_sky{
+		base_color = "#333355",
+		type = 'plain',
 		clouds = false
-	})
+	}
 
-	player:set_sun({
+	player:set_sun{
 		visible = false,
 		sunrise_visible = false
-	})
+	}
 
-	player:set_moon({
+	player:set_moon{
 		visible = false
-	})
+	}
 
-	player:set_stars({
+	player:set_stars{
 		visible = false
-	})
+	}
 
 	player:override_day_night_ratio(0.8)
 
-	player:set_physics_override({
+	player:set_physics_override{
 		speed = 3
-	})
+	}
+
+	-- Give fly and fast privs
+	local playername = player:get_player_name()
+	local pri = minetest.get_player_privs(playername)
+	pri["fly"] = true
+	pri["fast"] = true
+	minetest.set_player_privs(playername, pri)
 end)
